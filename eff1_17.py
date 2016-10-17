6147b4ffbd27e254bbbe9af8c7a17706147473b1
Daydup <hainuzsr@gmail.com>
2016-10-17 10:28:51 +0800

    eff1_17.py changed

--- eff1_17.py
+++ eff1_17.py
@@ -92,7 +92,7 @@ def normalize_defensive(numbers):
     for value in numbers:
         percent=100.0*value/total
         result.append(percent)
-    return result
+    return result 
 
 print normalize_defensive(ReadVisits(path))
 
