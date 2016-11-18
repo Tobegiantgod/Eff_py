7b3ee957dc5f864e8b0c6e95825745d1053589a9
Daydup <hainuzsr@gmail.com>
2016-11-18 10:49:39 +0800

    update it

--- eff3_28.py
+++ eff3_28.py
@@ -41,6 +41,9 @@ class BinaryNode(object):
 #用下标访问序列中的元素时bar[0],Python会把访问代码转译为:bar.__getitem__(0)
 
 class IndexableNode(BinaryNode):
+
+#深度优先，从左至右遍历二叉树。
+
     def _search(self, count, index):
         stack=[]
         found=None
@@ -61,12 +64,45 @@ class IndexableNode(BinaryNode):
             raise IndexError('Index out of range')
         return found.value
 
-
+    def __len__(self):
+        _, count = self._search(0, float('inf'))
+        return count
 tree = IndexableNode(10, left = IndexableNode(5,left = IndexableNode(2),right = IndexableNode(6, right = IndexableNode(7))), right=IndexableNode(15, left = IndexableNode(11)))
 
 
 
 print('Index 0 = %d' % tree[0])
 print('Index 3 = %d' % tree[3])
+print('Tree has %d nodes' % len(tree))
 
-                                                            
+#然而只实现__getitem__方法是不够的，它并不能使该类型支持我们想要的每一种序列操作。
+
+#想要拍使内置的len函数正常运行，就必须在自己定制的类型中实现另一个名叫__len__的特殊方法,如上，已添加。
+
+#实现了__len__方法之后，这个类的功能依然不完整。其他Python程序员还希望这个序列能够像list和tuple那样，提供count和index方法。我们可以使用内置的collection.abc模块。该模块定义了一系列抽象基类，它们提供了每一种容器类型所应该具备的常用方法，从这样的基类中继承了子类之后，如果忘记实现某个方法，那么collections.abc模块就会指出这个错误。
+
+
+#如果子类已经实现了抽象基类所要求的每个方法，那么基类就会自动提供剩下的那些方法。
+
+#Python3中才有collections.abc模块
+
+from collections.abc import Sequence
+
+class BetterNode(BinaryNode, Sequence):
+    
+    #__getitem__,__len__方法在Binarynode中已经实现过了，Sequence帮忙实现了count和index等方法。
+    
+    pass
+
+tree = BetterNode(10, left = BetterNode(5,left = BetterNode(2),right = BetterNode(6, right = BetterNode(7))), right=BetterNode(15, left = BetterNode(11)))
+
+print('Index of 7 is ', tree.index(7))
+print('Count of 10 is ', tree.count(10))
+
+
+
+
+
+
+
+
