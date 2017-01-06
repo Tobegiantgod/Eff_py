f6ef753f0c6a2ca1e67561ac54d2a6ea83a80b03
Daydup <hainuzsr@gmail.com>
2017-01-06 09:46:06 +0800

    add eff6_48.py

---
+++ eff6_48.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#    Author :  Daydup
+#    E-mail :  hainuzsr@gmail.com
+#    Date   :  170106 09:31:56
+##################################
+#6-48 学会安装由Python开发者所构建的模块
+##################################
+
+#Python有个中央仓库(https://pypi.python.org),里面存放着各种模块，以供程序开发者安装并使用。这些模块都是由Python社区构建并维护的，模块作者与大家一样，都是Python程序员。如果你碰到一个自己不太熟悉的编程难题，那就应该先去Python Package Index(简称PyPI)看看。PyPI里面的代码，很有可能会帮你尽快找到答案。
+
+#我们使用pip命令行工具来安装由Package Index提供的模块。
